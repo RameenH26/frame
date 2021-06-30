569cad6f402c79b499e99426ad5dcd2f17a5883d
RameenH26 <76857653+RameenH26@users.noreply.github.com>
2021-06-30 11:40:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas= createCanvas(400, 400);
+    canvas.position(180, 150);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+  //flipped camera code
+    push();
+  translate(width,0);
+  scale(-1, 1);
+  image(video, 0, 0, 400, 400);
+  pop();
+  
+
+  //frame code
+  fill('black')
+  rect(0, 0, 400, 20);
+  rect(0, 380, 400, 20);
+  rect(-1, 20, 20, 360);
+  rect(381, 20, 20, 360);
+ 
+}
+
+function take_snapshot() {
+    save('digital_frame.png');
+}
